b163a657ed3ec4a0c70eab9d85d3b7af333cb63b
merallo <davidmerallo76@gmail.com>
2016-06-17 14:05:17 +0200

    Mi primer commit

---
+++ app/src/main/java/com/davidmerallo/dell_09_039_pc/gitoroyect/MainActivity.java
@@ -0,0 +1,13 @@
+package com.davidmerallo.dell_09_039_pc.gitoroyect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
